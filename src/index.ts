2d6732a00318934e14b03e78a3de739ff18fd954
Tavish Pegram <tavish.pegram@apollographql.com>
2024-04-05 11:00:27 -0500

    typos

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ export async function main(): Promise<void> {
       ),
     );
 
-    // Assumption: The filepath is of the format `team/{team-name}` or `team/{team-name}/migrations` (for db migrations)
+    // Assumption: The filepath is of the format `teams/{team-name}` or `teams/{team-name}/migrations` (for db migrations)
     const team =
       core.getInput('files').split('/')[1]?.toLowerCase() ?? 'unknown';
     const isMigration = core.getInput('files').includes('migrations');
